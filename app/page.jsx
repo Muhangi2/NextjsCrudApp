4667edc1dd82194f114cbe148bad9155e2b486fa
Muhangi2 <eliodamutiba@gmail.com>
2023-09-06 18:02:10 +0300

    projectsetup

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <section className="w-full  flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover Prompts
+        <hr className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI PROMPTS</span>
+      </h1>
+      <p className="desc text-center">
+        Lets promote Ai using open source contribution
+      </p>
+    </section>
+  );
+}
+
+export default HomePage;
